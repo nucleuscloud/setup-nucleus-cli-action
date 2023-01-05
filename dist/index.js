44fd4e80d11e172f5711079ed929d15097bb4fbf
Alisha <alisha.kawaguchi@usenucleus.cloud>
2023-01-05 14:32:52 -0800

    for files

--- dist/index.js
+++ dist/index.js
@@ -267,7 +267,7 @@ function getDownloadUrl(version) {
             const obj = JSON.parse(body);
             const latestVersion = obj.name || obj.tag_name;
             if (!latestVersion) {
-                core.setFailed("failed to retrieve latest release");
+                core.setFailed('failed to retrieve latest release');
             }
             core.info(`Downloading latest Nucleus CLI version ${latestVersion}.`);
             return getUrl(latestVersion.replace('v', ''));
